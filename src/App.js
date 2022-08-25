3aec904dc1f3141b3469287d9fd1bc4573d42aba
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-08-25 10:30:42 +0545

    create initial project

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* make the two column  */}
+      <div className="row">
+        <div className="col-6">
+          {/* make the image responsive */}
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="col-6">
+          <h1>React Bootstrap</h1>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+      </div>
     </div>
-  );
+      );
 }
 
-export default App;
+      export default App;
