22f2de85fd221e77d1498ae03f6e46f914b913c1
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-08-28 10:52:05 +0545

    value pass to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Hello from './components/Hello';
+import City from './components/City'
 
 function App() {
   return (
     <div className="App">
-       <Hello name="Dipendra"/>
-       <Hello name="Dhiraj"/>
+      <City city="London" feature="beautiful"/>
+      <City city="New York" feature="fantastic"/>
+      <City city="Kathmandu" feature="majestic"/>
+      <City city="Beijing" feature="marvelous"/>
+
+      {/* <p>Hello, I am in London. It is beautiful.</p>
+      <p>Hello, I am in New York. It is fantastic.</p>
+      <p>Hello, I am in Kathmandu. It is majestic.</p>
+      <p>Hello, I am in Beijing. It is marvelous.</p> */}
+      
     </div>
   );
 }
