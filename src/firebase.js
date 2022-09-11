aa0c85866097859261879203fd5156bf747e85f4
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-09-11 11:11:17 +0545

    setup firebase config

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCIrIYXRTA2ei3dV9SaabZDzhD36lLDRcE",
+    authDomain: "todo-list-section-b.firebaseapp.com",
+    projectId: "todo-list-section-b",
+    storageBucket: "todo-list-section-b.appspot.com",
+    messagingSenderId: "142051602601",
+    appId: "1:142051602601:web:dbd1c3dcef60a4829e79b8",
+    measurementId: "G-VN2J30F020"
+  };
+  
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export default db;
