cf5129b76bf298c98da8b52262f414f6ef18d594
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-09-13 10:32:22 +0545

    finish firebase retrive code

--- src/components/TodoFirebase.jsx
+++ src/components/TodoFirebase.jsx
@@ -1,36 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './TodoFirebase.css'
 
 //import firebase files
 import firebase from 'firebase';
 import db from '../firebase';
 
+
 const TodoFirebase = () => {
     const [input, setInput] = useState('');
     const [todos, setTodos] = useState([]);
+
+    // when the app loads, 
+    // we need to listen to the database and 
+    // fetch new todos as they get added/removed
+    useEffect(() => {
+        //this code here... fires when the app.js loads
+        db.collection('todos')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => {
+                setTodos(snapshot.docs.map(
+                    doc => ({
+                        id: doc.id,
+                        todo: doc.data().todo
+                    })
+                ))
+            })
+    }, [input]);
 
     console.log(input)
     console.log(todos)
 
     //CREATE: Add a new todo and store it in the database (firestore)
-    function handleSubmit(){
+    function handleSubmit() {
         db.collection('todos').add({
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
             todo: input
         })
         setTodos([...todos, input]);
-        setInput('')  
-
+        setInput('')
     }
 
     //UPDATE: Update the individual items in the array having index
-    function updateTodo(){}
+    const updateTodo = () => {
+        db.collection('todos').doc(todos.id).set({
+        }, { merge: true })
+    }
 
     //DELETE: Delete the individual items in the array having index
-    function deleteTodo(){}
+    // function deleteTodo(){
+    //     db.collection('todos').doc(todos.id).delete();
+    // }
 
     //Remove All: It removes all the items in the array
-    function removeAll() {}
+    function removeAll() {
+        setTodos([])
+    }
 
     return (
         <div className='todo'>
@@ -60,8 +84,28 @@ const TodoFirebase = () => {
 
             <div className="todo-lists">
                 <h3>Task Lists</h3>
-                {/*  */}
-                <ul>
+                {/* display the items in the todos array */}
+                {todos.map(todo => (
+                    <ul>
+                        <div className="todo-list" key={todo.id}>
+                            <li>{todo.todo}</li>
+                            <button
+                                className='update'
+                                onClick={updateTodo}
+                            >
+                                Update
+                            </button>
+                            <button
+                                className='delete'
+                                onClick={event => db.collection('todos').doc(todo.id).delete()}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </ul>
+                ))}
+            </div>
+            {/* <ul>
                     {todos.map((todo) => (
                         <li>
                             {todo}
@@ -80,8 +124,7 @@ const TodoFirebase = () => {
                         </li>
                     ))
                     }
-                </ul>
-            </div>
+                </ul> */}
 
             <div className="todo-removeall">
                 <button
